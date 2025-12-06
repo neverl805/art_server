45475d22f219f8f083575fd02a0c93666441b875
Neverland <970821582@qq.com>
2025-12-06 19:43:52 +0800

    update

--- config.py
+++ config.py
@@ -1,8 +1,13 @@
 """应用配置"""
 from pathlib import Path
+import platform
+os_name = platform.system()
 
-# hcaptcha 日志数据库路径
-HCAPTCHA_LOGS_DB_PATH = Path(r"E:\js_reverse\new_hcaptcha\logs.db")
+if os_name == 'Windows':
+    # hcaptcha 日志数据库路径
+    HCAPTCHA_LOGS_DB_PATH = Path(r"E:\js_reverse\new_hcaptcha\logs.db")
+else:
+    HCAPTCHA_LOGS_DB_PATH = Path("/home/Neverland/mobile_hcaptcha/logs.db")
 
 # 如果需要使用相对路径，可以这样配置：
 # HCAPTCHA_LOGS_DB_PATH = Path(__file__).parent.parent / "js_reverse" / "new_hcaptcha" / "logs.db"
